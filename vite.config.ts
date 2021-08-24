26e46a66bb7c3749f4a3ef80ea44be8696d6d728
Huynh Duc Dung <dunghd.it@gmail.com>
2021-08-25 03:42:02 +0800

    :sparkles: Install vite plugin checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), checker({ typescript: true })],
 });
