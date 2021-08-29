d579bbda0a8d75791f8e06aeae073a3507ee409f
Huynh Duc Dung <dunghd.it@gmail.com>
2021-08-29 14:05:30 +0800

    :sparkles: Add Initializable to Box contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract Box {
+contract Box is Initializable{
     uint256 private _value;
+
+     function initialize(uint256 value) public initializer {
+         _value = value;
+    }
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 value);
