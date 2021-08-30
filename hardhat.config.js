ac38445b4ae2412e7d3ae5e8167d48a5e3b49f86
Huynh Duc Dung <dunghd.it@gmail.com>
2021-08-31 00:18:36 +0800

    :sparkles: Support BSC network

--- hardhat.config.js
+++ hardhat.config.js
@@ -57,6 +57,11 @@ module.exports = {
         process.env.MATIC_PROVIDER_URL || "https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/polygon/mumbai",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    bsctestnet: {
+      chainId: 97,
+      url: process.env.BSC_PROVIDER_URL || "https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/bsc/testnet",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
