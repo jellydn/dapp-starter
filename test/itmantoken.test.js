d606a77d4a10b1c2b92c643b110a8b64a47bcdb1
Huynh Duc Dung <dunghd.it@gmail.com>
2021-09-02 22:10:21 +0800

    :white_check_mark: Add unit test for ITManToken

---
+++ test/itmantoken.test.js
@@ -0,0 +1,36 @@
+const { expect } = require("chai");
+const { ethers, upgrades } = require("hardhat");
+
+describe("ITManToken", () => {
+  it("Should return the token name", async () => {
+    const ITManToken = await ethers.getContractFactory("ITManToken");
+    const itManToken = await upgrades.deployProxy(ITManToken);
+    await itManToken.deployed();
+
+    expect(await itManToken.name()).to.equal("ITManToken");
+  });
+
+  it("Should return the token symbol", async () => {
+    const ITManToken = await ethers.getContractFactory("ITManToken");
+    const itManToken = await upgrades.deployProxy(ITManToken);
+    await itManToken.deployed();
+
+    expect(await itManToken.symbol()).to.equal("ITM");
+  });
+
+  it("Should return decimals", async () => {
+    const ITManToken = await ethers.getContractFactory("ITManToken");
+    const itManToken = await upgrades.deployProxy(ITManToken);
+    await itManToken.deployed();
+
+    expect(await itManToken.decimals()).to.equal(18);
+  });
+
+  it("Should have total supply", async () => {
+    const ITManToken = await ethers.getContractFactory("ITManToken");
+    const itManToken = await upgrades.deployProxy(ITManToken);
+    await itManToken.deployed();
+
+    expect(await itManToken.totalSupply()).to.equal(ethers.BigNumber.from("1000000000000000000000000"));
+  });
+});
