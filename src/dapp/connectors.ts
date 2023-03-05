8ac9bcecfef6131a6145df883efa3326ffb52840
Huynh Duc Dung <dunghd.it@gmail.com>
2023-03-05 20:43:11 +0800

    chore(deps): update deps

--- src/dapp/connectors.ts
+++ src/dapp/connectors.ts
@@ -7,9 +7,9 @@ export const injected = new InjectedConnector({
 });
 
 const RPC_URLS: { [chainId: number]: string } = {
-  1: process.env.NEXT_PUBLIC_RPC_URL_1,
-  3: process.env.NEXT_PUBLIC_RPC_URL_3,
-  4: process.env.NEXT_PUBLIC_RPC_URL_4,
+  1: process.env.NEXT_PUBLIC_RPC_URL_1!,
+  3: process.env.NEXT_PUBLIC_RPC_URL_3!,
+  4: process.env.NEXT_PUBLIC_RPC_URL_4!,
   1337: "http://127.0.0.1:8545/",
 };
 
