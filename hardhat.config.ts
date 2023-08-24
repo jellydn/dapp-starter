f81d929441f5b63812fdad0f117b87e8669189c9
Huynh Duc Dung <dunghd.it@gmail.com>
2023-08-24 23:03:37 +0800

    chore: update deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
@@ -13,10 +12,8 @@ config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
-
-  // eslint-disable-next-line no-restricted-syntax
   for (const account of accounts) {
     console.log(account.address);
   }
@@ -36,33 +33,33 @@ module.exports = {
   typechain: {
     outDir: "src/types",
     target: "ethers-v5",
-    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
-    externalArtifacts: [], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    alwaysGenerateOverloads: false, // Should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: [], // Optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
   },
   networks: {
     hardhat: {
       chainId: 1337,
-      initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
+      initialBaseFeePerGas: 0, // Workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
     },
     ropsten: {
-      url: process.env.ROPSTEN_PROVIDER_URL || "",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      url: process.env.ROPSTEN_PROVIDER_URL ?? "",
+      accounts: [process.env.PRIVATE_KEY ?? ""].filter(Boolean),
     },
     tomotestnet: {
       chainId: 89,
-      url: process.env.TOMO_PROVIDER_URL || "https://rpc.testnet.tomochain.com",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      url: process.env.TOMO_PROVIDER_URL ?? "https://rpc.testnet.tomochain.com",
+      accounts: [process.env.PRIVATE_KEY ?? ""].filter(Boolean),
     },
     matictestnet: {
       chainId: 80001,
       url:
-        process.env.MATIC_PROVIDER_URL || "https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/polygon/mumbai",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        process.env.MATIC_PROVIDER_URL ?? "https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/polygon/mumbai",
+      accounts: [process.env.PRIVATE_KEY ?? ""].filter(Boolean),
     },
     bsctestnet: {
       chainId: 97,
-      url: process.env.BSC_PROVIDER_URL || "https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/bsc/testnet",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      url: process.env.BSC_PROVIDER_URL ?? "https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/bsc/testnet",
+      accounts: [process.env.PRIVATE_KEY ?? ""].filter(Boolean),
     },
   },
   gasReporter: {
